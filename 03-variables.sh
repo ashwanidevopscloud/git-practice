d9c98c6f11602ca5f3c2800a8774278a68107a44
Ashwani <ashwaniankireddypalle@gmail.com>
2024-08-27 17:18:55 +0530

    adding variables

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Sai
+
+echo "$PERSON1::Hi ${PERSON2}, how ae yo?"
+echo "$PERSON2::hello $PERSON1, I AM FINE, what abot yo?"
+echo "$PERSON1::i am doing good ${PESON2},what's going on?"
+echo "$PERSON2::I stated a learning shell script $PERSON1"
