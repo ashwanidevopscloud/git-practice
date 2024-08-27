cd7871de33e8dea2c1186ad4db5fbbe5c08951a6
Ashwani <ashwaniankireddypalle@gmail.com>
2024-08-27 18:18:38 +0530

    adding array vales

---
+++ 06-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+FRuITS=("APPLE" "KIWI" "ORANGE") #ARRAY
+
+
+echo "first fruit is :: ${FRuITS[0]}"
+echo "first fruit is :: ${FRuITS[1]}"
+echo "first fruit is :: ${FRuITS[3]}"
+
+echo "ALL fruit ARE :: ${FRuITS[@]}"
