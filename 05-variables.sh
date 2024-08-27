e06acad99d6ae6967b14f2cb776355059e17c866
Ashwani <ashwaniankireddypalle@gmail.com>
2024-08-27 18:00:33 +0530

    adding name & password

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter your username::"
+
+read username
+
+echo "username entered is ::${username}"
+
+echo "Please enter your password::"
+read  password
+echo "password entered is:: ${password}"
