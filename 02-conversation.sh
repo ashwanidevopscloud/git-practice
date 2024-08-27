c5627a0409cf24a752c00a0934da2f713ed53859
Ashwani <ashwaniankireddypalle@gmail.com>
2024-08-27 17:07:31 +0530

    added conversation script file

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh::Hi Sai, How are yo?"
+echo "Sai::Hello Ramesh, I am fine, how are yo doing?"
+echo "Ramesh::I am doing good Sai , what's going on"
+echo "Sai::I started learning shell script Ramesh"
