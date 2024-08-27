fe745ef523e1a5076a2b0e1b2df4fc0c69d6ccfc
Ashwani <ashwaniankireddypalle@gmail.com>
2024-08-27 17:25:43 +0530

    adding argments

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1::Hi ${PERSON2}, how ae yo?"
+echo "$PERSON2::hello $PERSON1, I AM FINE, what abot yo?"
+echo "$PERSON1::i am doing good ${PERSON2},what's going on?"
+echo "$PERSON2::I stated a learning shell script $PERSON1"
+
