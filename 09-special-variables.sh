246787627540e8d80105b4cf69772c595df0db3d
Ashwani <ashwaniankireddypalle@gmail.com>
2024-08-28 06:52:44 +0530

    adding special vaiables vales

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 echo "all vaiables passed to the script:$@"
 echo "nmber of vaiables passed::$#"
+echo "scipt name::$0"
+echo "crrent pesent working directory ::$PWD"
+echo "home directoy of crent ser:$HOME"
+echo "PID of scipt ececting now:$$"
+sleep 200 &
+echo "PID of last backgond command ::$!"
