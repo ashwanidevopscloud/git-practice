937866ed2d550952cec3838a13c65e0ac364228a
Ashwani <ashwaniankireddypalle@gmail.com>
2024-08-28 06:53:53 +0530

    adding special vaiables vales

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -5,5 +5,5 @@ echo "scipt name::$0"
 echo "crrent pesent working directory ::$PWD"
 echo "home directoy of crent ser:$HOME"
 echo "PID of scipt ececting now:$$"
-sleep 200 &
+sleep 20000 &
 echo "PID of last backgond command ::$!"
