7328c403d973626132c5b41250d5a30c61bb997d
Ashwani <ashwaniankireddypalle@gmail.com>
2024-08-30 15:33:51 +0530

    if condiction

--- 11-installation.sh
+++ 11-installation.sh
@@ -6,6 +6,8 @@ echo " user id is: $uSERID"
 if [ $uSERID -ne 0 ]
 then
    echo "Please rn the scrpit with root priveleges"
+   exit 1
 else
-   echo " i am rnning with root priveleges"
+   echo " i am not rnning with root priveleges"
 fi
+dnf install git -y
