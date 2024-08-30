ede4c7b1103b95dd3722bc3ecbf559e157c7b790
Ashwani <ashwaniankireddypalle@gmail.com>
2024-08-30 13:56:46 +0530

    condiction if

--- 10-condiction.sh
+++ 10-condiction.sh
@@ -4,9 +4,9 @@ NuMBER1=$1
 
 
 if [ $NuMBER1 -gt 20 ] 
-than
-    echo "given NuMBER:$NuMBER1 is greater than 20"
+then
+    echo "given NuMBER:: $NuMBER1 is greater than 20"
 else
-    echo "given NuMBER:: $NuMBER1 is lessthan 20"
+     echo "given NuMBER:: $NuMBER1 is lessthan 20"
 fi
  
