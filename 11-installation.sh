f5eb5e8860202fa77fd808a3a249bb05246a4c16
Ashwani <ashwaniankireddypalle@gmail.com>
2024-08-31 07:20:47 +0530

    installing git

--- 11-installation.sh
+++ 11-installation.sh
@@ -23,3 +23,19 @@ then
 else
    echo "Git is already installed, nothing to do......."
 fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+    echo "mysql is not installed, going to install it ......"
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "mysql installation is not sccess..... check it"
+        exit 1
+    else
+        echo "mysql installation is scess....completed"
+    fi
+else
+   echo "mysql is already installed, nothing to do......"
+fi
