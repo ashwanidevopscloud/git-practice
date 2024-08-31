ad588e91bc7a5d04da1774ded13d3ccc7cb1c704
Ashwani <ashwaniankireddypalle@gmail.com>
2024-08-31 07:12:22 +0530

    installing git

--- 11-installation.sh
+++ 11-installation.sh
@@ -13,6 +13,12 @@ if [ $? -ne 0 ]
 then
    echo "git is not installed, going to install it ....."
    dnf install gitttt -y
+   if [$? -ne 0 ]
+   then
+      echo "Git installation is not scess.....check it"
+      exit 1
+    else
+      echo "Git installation is scesss....."
 else
    echo "Git is already installed, nothing to do......."
 fi
