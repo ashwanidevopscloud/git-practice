349e71c63846b4517b44620bcaec472555b1966d
Ashwani <ashwaniankireddypalle@gmail.com>
2024-08-31 07:26:33 +0530

    installing git

--- 11-installation.sh
+++ 11-installation.sh
@@ -8,21 +8,6 @@ then
    echo "Please run the script with root privelliges"
    exit 1
 fi
-dnf list installed gittttttttttt
-if [ $? -ne 0 ]
-then
-   echo "git is not installed, going to install it ....."
-   dnf install gitttt -y
-   if [ $? -ne 0 ]
-   then
-      echo "Git installation is not scess.....check it"
-      exit 1
-    else
-      echo "Git installation is scesss....."
-    fi
-else
-   echo "Git is already installed, nothing to do......."
-fi
 
 dnf list installed mysql
 if [ $? -ne 0 ]
