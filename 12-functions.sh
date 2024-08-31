8148fe2fb390b6459f43599bf93aea89e98d0b34
Ashwani <ashwaniankireddypalle@gmail.com>
2024-08-31 08:27:36 +0530

    fnction adding

--- 12-functions.sh
+++ 12-functions.sh
@@ -18,37 +18,26 @@ then
 else
     echo "I have root access.... i will procide with that....."
 fi
-VALIDATE $? "Listing git"
 
-# dnf list installed git
 
-# if [ $? -ne 0 ]
-# then
-#    echo "Git is not installed....., going to install it"
-#    dnf install git -y
-#    if [ $? -ne 0 ]
-#    then
-#        echo "git installation is failes.....check it..."
-#        exit 1
-#     else
-#        echo "git installation in scesss....completed"
-#     fi
-# else
-#    echo "Git is already installed.... nothing to do"
-# fi
+dnf list installed git
 
-# dnf list installed mysql
-# if [ $? -ne 0 ]
-# then
-#     echo "mysql is not installed.... going to install it"
-#     dnf install mysql -y
-#     if [ $? -ne 0 ]
-#     then
-#         echo "mysql installation is failed...check it"
-#         exit 1
-#     else
-#         echo "mysql installation is scess... completed"
-#     fi
-# else
-#     echo "mysql is aleady installed.... nothing tod do"
-# fi
+if [ $? -ne 0 ]
+then
+   echo "Git is not installed....., going to install it"
+   dnf install git -y
+   VALIDATE $? "installing git"
+else
+   echo "Git is already installed.... nothing to do"
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    echo "mysql is not installed.... going to install it"
+    dnf install mysql -y
+    VALIDATE $? "installing mysql"
+    
+else
+    echo "mysql is aleady installed.... nothing tod do"
+fi
