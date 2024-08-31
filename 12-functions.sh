3d4d5bfb07f7fe8284e800f50da12372f09e8441
Ashwani <ashwaniankireddypalle@gmail.com>
2024-08-31 08:16:48 +0530

    fnction adding

--- 12-functions.sh
+++ 12-functions.sh
@@ -28,7 +28,7 @@ fi
 dnf list installed mysql
 if [ $? -ne 0 ]
 then
-    echo mysql is not installed.... going to install it"
+    echo "mysql is not installed.... going to install it"
     dnf install mysql -y
     if [ $? -ne 0 ]
     then
