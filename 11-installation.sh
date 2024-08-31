cdf991399b1387d15a7aea0129251bc51381c1c9
Ashwani <ashwaniankireddypalle@gmail.com>
2024-08-31 06:42:59 +0530

    installing git

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-#uSERID=$(id -u)
-#echo " user id is: $uSERID"
-  #dnf install git -y
-#if [ $uSERID -ne 0 ]
-#then
-   #echo "Please rn the scrpit with root priveleges"
-   #exit 1
-#else
-   #echo "please i have root access"
-#fi
-#dnf install git -y
+uSERID=$(id -u)
 
-ls -ltr
-lsss -lt
-ls -l
+
+echo "userid is :: $uSERID"
+
+if [ $uSERID -ne 0]
+then
+    echo "To take yo don't have root super priveliges access"
+    exit 1
+else
+    echo "i have super priveliges access"
+fi
+dnf install git -y
