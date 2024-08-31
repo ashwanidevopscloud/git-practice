8be4d10128b669f0467a2038d8a2644fb6eff893
Ashwani <ashwaniankireddypalle@gmail.com>
2024-08-31 15:42:41 +0530

    rediectos log file

--- 15.loops.sh
+++ 15.loops.sh
@@ -7,6 +7,7 @@ VALIDATE(){
     if [ $1 -ne 0 ]
     then
        echo -e "$2 is $R FAILED....check with that $N"
+       exit 1
     else
        echo -e "$2 is $G SUCCESS...... completed $N"
     fi
